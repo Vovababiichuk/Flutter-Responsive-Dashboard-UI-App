e1d71d4c02b950eea6c760f5800c6030072eedb8
VVVV BBBB <Volodymyr>
2024-03-12 23:17:51 +0200

    added base maket

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -5,6 +5,22 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(flex: 2, child: Container(color: Colors.red)),
+            Expanded(
+              flex: 7,
+              child: Container(color: Colors.blue),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(color: Colors.green),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
