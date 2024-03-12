60ccd02f9c94e0cec3b8d9d14b2b38c593e47250
VVVV BBBB <Volodymyr>
2024-03-12 22:40:15 +0200

    created base structure project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_ui_app/const/constant.dart';
+import 'package:responsive_dashboard_ui_app/screens/main_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        brightness: Brightness.dark,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
