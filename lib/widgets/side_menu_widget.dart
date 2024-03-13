d8f74595b73774c558a91aff6e3777e91a9e82eb
VVVV BBBB <Volodymyr>
2024-03-13 18:34:12 +0200

    added sidebar

--- lib/widgets/side_menu_widget.dart
+++ lib/widgets/side_menu_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_ui_app/const/constant.dart';
 import 'package:responsive_dashboard_ui_app/data/side_menu_data.dart';
 
 class SideMenuWidget extends StatefulWidget {
@@ -9,10 +10,51 @@ class SideMenuWidget extends StatefulWidget {
 }
 
 class _SideMenuWidgetState extends State<SideMenuWidget> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final data = SideMenuData();
 
-    return Container();
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+        child: ListView.builder(
+          itemCount: data.menu.length,
+          itemBuilder: (context, index) => buildMenuEntry(data, index),
+        ));
+  }
+
+  Widget buildMenuEntry(SideMenuData data, int index) {
+    final isSelected = selectedIndex == index;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(6.0)),
+        color: isSelected ? selectionColor : Colors.transparent,
+      ),
+      child: InkWell(
+        onTap: () => setState(() {
+          selectedIndex = index;
+        }),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 7),
+              child: Icon(
+                data.menu[index].icon,
+                color: isSelected ? Colors.black : Colors.grey,
+              ),
+            ),
+            Text(data.menu[index].title,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: isSelected ? Colors.black : Colors.grey,
+                  fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
