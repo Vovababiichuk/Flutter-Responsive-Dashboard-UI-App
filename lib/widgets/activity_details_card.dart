4784e71efaff16cf7e48d7c6b9848062935731bb
VVVV BBBB <Volodymyr>
2024-03-14 22:16:49 +0200

    added cards

--- lib/widgets/activity_details_card.dart
+++ lib/widgets/activity_details_card.dart
@@ -19,7 +19,36 @@ class ActivityDetailsCard extends StatelessWidget {
         mainAxisSpacing: 12.0,
       ),
       itemBuilder: (context, index) => CustomCard(
-        child: Container(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              healthDetails.healthData[index].icon,
+              width: 30,
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 4),
+              child: Text(
+                healthDetails.healthData[index].value,
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                )
+              ),
+            ),
+            Text(
+              healthDetails.healthData[index].title,
+              style: const TextStyle(
+                fontSize: 13,
+                color: Colors.grey,
+                fontWeight: FontWeight.normal,
+              )
+            )
+          ],
+        ),
       ),
     );
   }
