322b5aed424aafd0f2f8c955beecbd6f4980c8c5
VVVV BBBB <Volodymyr>
2024-03-14 23:44:30 +0200

    added diagrams

--- lib/widgets/dashboard_widget.dart
+++ lib/widgets/dashboard_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_ui_app/widgets/activity_details_card.dart';
+import 'package:responsive_dashboard_ui_app/widgets/bar_graph_widget.dart';
 import 'package:responsive_dashboard_ui_app/widgets/header_widget.dart';
 import 'package:responsive_dashboard_ui_app/widgets/line_chart_card.dart';
 
@@ -8,15 +9,22 @@ class DashboardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(height: 18),
-        HeaderWidget(),
-        SizedBox(height: 18),
-        ActivityDetailsCard(),
-        SizedBox(height: 18),
-        LineChartCard(),
-      ]
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            SizedBox(height: 18),
+            HeaderWidget(),
+            SizedBox(height: 18),
+            ActivityDetailsCard(),
+            SizedBox(height: 18),
+            LineChartCard(),
+            SizedBox(height: 18),
+            BarGraphCard(),
+          ]
+        ),
+      ),
     );
   }
 }
