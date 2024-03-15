1c64d3b682f77764b3548a7d92ca5a21236ba677
VVVV BBBB <Volodymyr>
2024-03-15 16:02:48 +0200

    added card details

--- lib/widgets/summary_widget.dart
+++ lib/widgets/summary_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard_ui_app/widgets/pie_chart_widget.dart';
+import 'package:responsive_dashboard_ui_app/widgets/symmary_details_widget.dart';
 
 class SummaryWidget extends StatelessWidget {
   const SummaryWidget({super.key});
@@ -10,6 +11,11 @@ class SummaryWidget extends StatelessWidget {
       children: [
         SizedBox(height: 20),
         Chart(),
+        Text(
+          'Summary',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 16),
+        SummaryDetails(),
       ],
     );
   }
